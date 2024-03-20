aed42530c3ac264950b30df0ef7b53a3faee536d
debnathk <debntahk1997@gmail.com>
2024-03-20 18:48:27 -0400

    Create bash script to run roberta_large, install dependencies within the venv

---
+++ project/Com2Sense/run_train_roberta_large.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=com2sense_roberta
+#SBATCH --output output_train_roberta_large.log
+#SBATCH --error error_train_roberta_large.log
+#SBATCH --partition gpu
+#SBATCH --mem=32G          
+#SBATCH --gres=gpu:1
+
+echo "Date"
+date
+
+start_time=$(date +%s)
+
+# Load the necessary modules
+# module purge
+module load cuda/12.3  # Change this to the appropriate CUDA version
+# module load cudnn/8.0.4   # Change this to the appropriate cuDNN version
+# module load anaconda/2020.11  # Change this to the appropriate Anaconda version
+
+# Activate Python environment
+source activate your_conda_environment  
+
+# Run Python script
+python3 main.py --mode train \
+--expt_dir ./results_log/com2sense \
+--expt_name roberta_large \
+--model roberta-large \
+--dataset com2sense \
+--run bs_16 \
+--batch_size 16 \
+--seq_len 128
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+echo "Elapsed time: $elapsed_time seconds"
